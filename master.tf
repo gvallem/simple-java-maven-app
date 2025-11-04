5e2885a00b69ff9c8b0f0d719ac143bb5d7cdd43
Guillermo Valle <gvamen@hotmail.com>
2025-11-04 12:04:25 -0600

    Refactor Terraform configuration for GCP resources
    Updated Terraform configuration for GCP resources including provider version, backend settings, network and subnetwork definitions, firewall rules, and VM instance setup. Changed from NGINX to Apache and updated output variables.

--- master.tf
+++ master.tf
@@ -1,71 +1,145 @@
 terraform {
+  required_version = ">= 1.4"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 5.40"
     }
   }
+
+  # Backend GCS: usa tu bucket EXISTENTE
   backend "gcs" {
-    bucket = "tf-state-UNICO"
-    prefix = "envs/dev"
+    bucket = "tf-state-gvm"
+    prefix = "tfstate/ex3"
   }
 }
 
+########################
+# Variables y defaults #
+########################
+variable "project_id" {
+  type        = string
+  description = "ID del proyecto GCP"
+  default     = "gcp-mod1-lab-gobierno"    # <-- cambia si es necesario
+}
+
+variable "region" {
+  type        = string
+  description = "Región GCP"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zona GCP (solo se usa en la VM)"
+  default     = "us-central1-a"
+}
+
+variable "network_name" {
+  type        = string
+  description = "Nombre de la VPC"
+  default     = "vpc-web"
+}
+
+variable "subnet_cidr" {
+  type        = string
+  description = "CIDR de la subred"
+  default     = "10.10.0.0/24"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Tipo de máquina"
+  default     = "e2-micro"
+}
+
+##############
+# Provider   #
+##############
 provider "google" {
   project = var.project_id
   region  = var.region
-  zone    = var.zone
+  # Nota: "zone" NO va aquí (para evitar duplicidad). La ponemos en el recurso VM.
 }
 
-resource "google_compute_network" "vpc_web" {
-  name                    = "vpc-web"
+#############################
+# Red, subred y firewall    #
+#############################
+resource "google_compute_network" "vpc" {
+  name                    = var.network_name
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "subnet_web" {
-  name          = "subnet-web"
-  ip_cidr_range = "10.10.0.0/24"
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.network_name}-subnet"
+  ip_cidr_range = var.subnet_cidr
   region        = var.region
-  network       = google_compute_network.vpc_web.id
+  network       = google_compute_network.vpc.id
 }
 
 resource "google_compute_firewall" "allow_http_https" {
-  name    = "fw-allow-http-https"
-  network = google_compute_network.vpc_web.name
-  allow { protocol = "tcp" ports = ["80","443"] }
-  target_tags   = ["http-server"]     # amarrada a etiqueta
-  direction     = "INGRESS"
+  name    = "${var.network_name}-allow-web"
+  network = google_compute_network.vpc.name
+
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
   source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web"]
 }
 
+#############################
+# VM Debian + Apache (HTTP) #
+#############################
 resource "google_compute_instance" "vm_web" {
-  name         = "vm-web-1"
-  machine_type = "e2-micro"
+  name         = "vm-web"
+  machine_type = var.machine_type
   zone         = var.zone
-  tags         = ["http-server"]
-  boot_disk { initialize_params { image = "projects/debian-cloud/global/images/family/debian-12" } }
-  network_interface {
-    subnetwork = google_compute_subnetwork.subnet_web.name
-    access_config {}
+  tags         = ["web"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/family/debian-12"
+    }
   }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.name
+
+    access_config {
+      # IP pública efímera
+    }
+  }
+
   metadata_startup_script = <<-EOT
     #!/bin/bash
+    set -e
     apt-get update -y
-    apt-get install -y nginx
-    echo "<h1>Hello from Terraform + NGINX</h1>" > /var/www/html/index.nginx-debian.html
-    systemctl enable nginx
-    systemctl restart nginx
+    apt-get install -y apache2
+    echo "Hola desde Terraform en ${HOSTNAME}" > /var/www/html/index.html
+    systemctl enable apache2
+    systemctl restart apache2
   EOT
 }
 
-output "vm_ip" {
-  value = google_compute_instance.vm_web.network_interface[0].access_config[0].nat_ip
+############
+# Outputs  #
+############
+output "vm_public_ip" {
+  description = "IP pública de la VM web"
+  value       = try(google_compute_instance.vm_web.network_interface[0].access_config[0].nat_ip, null)
 }
-variables.tf
-variable "project_id" { type = string }
-variable "region"     { type = string  default = "us-central1" }
-variable "zone"       { type = string  default = "us-central1-a" }
-terraform.tfvars
-project_id = "gcp-mod1-lab-gobierno"
-region     = "us-central1"
-zone       = "us-central1-a"
+
+output "http_url" {
+  description = "URL HTTP de prueba"
+  value = (
+    try(google_compute_instance.vm_web.network_interface[0].access_config[0].nat_ip, null) != null
+    ? "http://${google_compute_instance.vm_web.network_interface[0].access_config[0].nat_ip}"
+    : null
+  )
+}
