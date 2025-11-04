d9ed18761c46a636fd027fdcad563ea76858bd50
Guillermo Valle <gvamen@hotmail.com>
2025-11-03 18:29:13 -0600

    Clean up AppTest by removing unused code
    Removed unused imports and test methods.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,11 +1,6 @@
 package com.mycompany.app;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.After;
-import static org.junit.Assert.*;
+
 
 /**
  * Unit test for simple App.
@@ -13,21 +8,6 @@ import static org.junit.Assert.*;
 public class AppTest
 {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @Test
-    public void testAppConstructor() {
-     
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
+    //Test go here
 
 }
